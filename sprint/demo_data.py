56e383f9faea2e1f78f20ff933d912cb95baf175
vishnuyar <vardhanvishnu@gmail.com>
2019-10-11 23:21:02 +0530

    adding after final changes

--- sprint/demo_data.py
+++ sprint/demo_data.py
@@ -19,6 +19,49 @@ def data_operations(conn,query):
     #On successful completion return the results
     return results
 
+
+
+def count_query(connect):
+    """ 
+    Function to find the number of rows in the demo table
+    Expected Output :The number of rows in demo table is 6 
+    
+    """
+    #How many rows in the demo table
+    count_query = """SELECT COUNT(*) FROM demo; """
+    #checking for 3 rows
+    result = data_operations(connect,count_query)
+    for row in result:
+        print(f'The number of rows in demo table is {row[0]}')
+
+def xy_query(connect):
+    """ 
+    Function for finding the number of rows where x and y are atleast equal to 5
+    Expected Output :The number of rows where x and y is atleast 5: 4 
+    
+    """
+    #How many rows where x and y are atleast 5
+    xy_query = """ SELECT COUNT(*) FROM demo WHERE x >= 5 AND y >= 5; """
+    #checking for atleast x y having value 5
+    result = data_operations(connect,xy_query)
+    for row in result:
+        print(f'The number of rows where x and y is atleast 5: {row[0]}')
+
+
+def y_unique(connect):
+    """ 
+    Function for finding the Number of Unique values of Y
+    Expected output : The number of distinct values of y : 2 
+    
+    """
+    #Query for unique values of y
+    y_unique = """ SELECT COUNT(DISTINCT y) FROM demo ; """
+    #checking for distinct values of y
+    result = data_operations(connect,y_unique)
+    for row in result:
+        print(f'The number of distinct values of y : {row[0]}')
+    
+
 #Create a connection within a try excecpt block to pass errors without causing exception
 try:
     #Creating a database with name demo_data
@@ -43,30 +86,10 @@ try:
     data_operations(connect,insert_query)
 
     #Now checking the demo table for data
-    #How many rows in the demo table
-    count_query = """SELECT COUNT(*) FROM demo; """
-    #How many rows where x and y are atleast 5
-    xy_query = """ SELECT COUNT(*) FROM demo WHERE x >= 5 AND y >= 5; """
-    #Query for unique values of y
-    y_unique = """ SELECT COUNT(DISTINCT y) FROM demo ; """
+    count_query(connect) # Number of rows in the table
+    xy_query(connect)  # Number of rows with x y values more than 5
+    y_unique(connect)  # Number of distinct y values in the table
     
-    #checking for 3 rows
-    result = data_operations(connect,count_query)
-    for row in result:
-        print(f'The number of rows in demo table is {row[0]}')
-    
-    #checking for atleast x y having value 5
-    result = data_operations(connect,xy_query)
-    for row in result:
-        print(f'The number of rows where x and y is atleast 5: {row[0]}')
-    
-    #checking for distinct values of y
-    result = data_operations(connect,y_unique)
-    for row in result:
-        print(f'The number of distinct values of y : {row[0]}')
-    
-    
-
 except:
     pass
 
--- sprint/northwind.py
+++ sprint/northwind.py
@@ -19,41 +19,96 @@ def data_operations(conn,query):
     #On successful completion return the results
     return results
 
-#Creating a try catch block to safely operation on database
-try:
-    #Creating a connection to the Northwind database
-    connect = sqlite3.connect('northwind_small.sqlite3')
-
+def top_10_expensive_items(connect):
+    """ 
+    Function to find the Top 10 expensive items per unit price
+    Expected Output 
+    The 10 Most expensive Items in the database are:
+        Côte de Blaye
+        Thüringer Rostbratwurst
+        Mishi Kobe Niku
+        Sir Rodney's Marmalade
+        Carnarvon Tigers
+        Raclette Courdavault
+        Manjimup Dried Apples
+        Tarte au sucre
+        Ipoh Coffee
+        Rössle Sauerkraut
+     """
     #query for ten most expensive items in the database
-    expensive_query = """ SELECT ProductName, UnitPrice 
+    expensive_query = """ SELECT ProductName 
                             FROM Product 
                             ORDER BY UnitPrice DESC LIMIT 10 """
     result = data_operations(connect,expensive_query)
     print("The 10 Most expensive Items in the database are:\n")
     for row in result:
-        print(f'{row[0]} : {row[1]}')
+        print(f'{row[0]}')
+
+def avg_age_employee(connect):
+    """ 
+    Function to find the average age of the employee at the time of hire
+    Expected Output 
+    The Averag age of the employee at the time of hire is:37.22
+    """
     #Query for the Avg age of the employee at the time of hiring
     avg_age_employee = """ SELECT AVG(HireDate - BirthDate) FROM Employee; """
     result = data_operations(connect,avg_age_employee)
     print("\n")
     for row in result:
-        print(f'The Averag age of the employee is:{row[0]:0.2f}')
+        print(f'The Averag age of the employee at the time of hire is:{row[0]:0.2f}')
+
+def avg_employee_age_bycity(connect):
+    """ 
+    Function to find the average age of employees at the time of hire by city
+    Expected Output 
+    The Averag age of the employee at the time of hire by City is :
+    Kirkland : 29.0
+    London : 32.5
+    Redmond : 56.0
+    Seattle : 40.0
+    Tacoma : 40.0
+    """
     #Query for avg age of the employee by city
     avg_employee_age_bycity = """ SELECT city, AVG(HireDate - BirthDate)
                                   FROM Employee GROUP BY City; """
     result = data_operations(connect,avg_employee_age_bycity)
-    print("\nThe Averag age of the employee by City is :")
+    print("\nThe Averag age of the employee at the time of hire by City is :")
     for row in result:
         print(f'{row[0]} : {row[1]}')
+
+def expensive_items_supplier(connect):
+    """ 
+    Function to find the Top 10 expensive items per unit price and the Name of the Supplier
+    Expected Output 
+    The 10 Most expensive Items in the database Supplier Names followed by Item Name:
+    Aux joyeux ecclésiastiques : Côte de Blaye
+    Plutzer Lebensmittelgroßmärkte AG : Thüringer Rostbratwurst
+    Tokyo Traders : Mishi Kobe Niku
+    Specialty Biscuits, Ltd. : Sir Rodney's Marmalade
+    Pavlova, Ltd. : Carnarvon Tigers
+    Gai pâturage : Raclette Courdavault
+    G'day, Mate : Manjimup Dried Apples
+    Forêts d'érables : Tarte au sucre
+    Leka Trading : Ipoh Coffee
+    Plutzer Lebensmittelgroßmärkte AG : Rössle Sauerkraut
+    """
     #Query for Top 10 expensive items along with Suppliet names
-    expensive_query_supplier = """ SELECT CompanyName,ProductName, UnitPrice 
+    expensive_query_supplier = """ SELECT CompanyName,ProductName 
                             FROM Product, Supplier 
                             WHERE Supplier.Id = Product.SupplierId 
                             ORDER BY UnitPrice DESC LIMIT 10"""
     result = data_operations(connect,expensive_query_supplier)
-    print("\nThe 10 Most expensive Items in the database with Supplier Names are:\n")
+    print("\nThe 10 Most expensive Items in the database with Supplier Names followed by Item Name:\n")
     for row in result:
-        print(f'{row[0]} : {row[1]}: {row[2]}')
+        print(f'{row[0]} : {row[1]}')
+
+def largest_category(connect):
+    """ 
+    Function to find the Top category with largest unique products
+    Expected Output 
+    The Category with largest unique products is :Confections 
+    
+    """
     #Query for the name of category with largest number of unique products
     largest_category = """ SELECT CategoryName FROM Category WHERE id = (
                             SELECT CategoryId FROM Product 
@@ -63,6 +118,14 @@ try:
     print("\n")
     for row in result:
         print(f'The Category with largest unique products is :{row[0]}')
+
+def most_territories_employee(connect):
+    """
+    Function to find the Top 10 expensive items per unit price
+    Expected Output 
+    The Employee with most territories is  :Robert King 
+    
+    """
     #Query for name of the Employee who has the most territories
     most_territories_employee = """ SELECT FirstName,LastName FROM Employee WHERE id = (
                                     SELECT EmployeeId FROM EmployeeTerritory 
@@ -73,6 +136,22 @@ try:
     print("\n")
     for row in result:
         print(f'The Employee with most territories is  :{row[0]} {row[1]}')
+
+
+
+
+#Creating a try catch block to safely operation on database
+try:
+    #Creating a connection to the Northwind database
+    connect = sqlite3.connect('northwind_small.sqlite3')
+
+    top_10_expensive_items(connect)
+    avg_age_employee(connect)
+    avg_employee_age_bycity(connect)
+    expensive_items_supplier(connect)
+    largest_category(connect)
+    most_territories_employee(connect)
+    
 
 except :
     pass
